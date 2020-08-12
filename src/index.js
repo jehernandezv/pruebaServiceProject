726c34b660065cba52f70a205d663f4be8ca7dfe
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-08-12 16:10:24 -0500

    upload image a cloudinary

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
+require('dotenv').config();
 var express = require('express');
 var cors = require('cors');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 const multer = require('multer');
+const path = require('path');
+const cloudinary = require('cloudinary');
+const fs = require('fs-extra');
+const uuid = require('uuid').v4;
+
 //require('./database');
 //const Patient = require('./models/Patient');
-const uuid = require('uuid').v4;
+
 var port = process.argv[2];
 var numServer = process.argv[3];
-
-
-
 var app = express();
-const storage = null;
+const storage = multer.diskStorage({
+    destination:path.join(__dirname,'public/uploads'),
+    filename:(req,file,cb) => {
+        cb(null,uuid()+path.extname(file.originalname))
+    }
+});
 
 
 app.use(cors());
@@ -21,6 +29,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(multer({storage}).single('image'));
 app.use(bodyParser.json());
+cloudinary.config({
+    cloud_name: 'drh8926tv',
+    api_key: '296114521285185',
+    api_secret: 'qBb8TnYd7X9f7LdnyEbXjs5SqMs'
+});
 
 app.get('/',function(req,res){
     res.json({
@@ -44,12 +57,13 @@ app.get('/:city',function(req,res){
 });
 
 //registra casos
-app.post('/addCovid', (req,res) => {
+app.post('/addCovid',async (req,res) => {
     try{
-
         console.log(req.body.name);
         console.log(req.file.originalname);
         console.log(req.body.city);
+       const upload = await cloudinary.v2.uploader.upload(req.file.path);
+       console.log(upload);
 
         /*
         const patient = new Patient({
@@ -58,7 +72,9 @@ app.post('/addCovid', (req,res) => {
             UrlImage:uuid()
         });*/
 
-       // patient.save();
+       //await patient.save();
+       //eliminando imagen del server
+        await fs.unlink(req.file.path);
         console.log('Se ha guardado un paciente')
 
         res.json({
