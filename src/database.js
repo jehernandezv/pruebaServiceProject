97fc8f7d0171f008fd887109b1e965e26fda8172
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-08-12 11:39:08 -0500

    ipmongo

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/projectFinal', {
+mongoose.connect('mongodb://192.168.100.4/projectFinal', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('db is conected')).catch(err => console.log(err));
